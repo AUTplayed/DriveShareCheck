d8663d8f682b4d4b7d849187a71b0abd571cf394
Philipp <fips.hem@gmail.com>
2016-10-14 18:37:07 +0200

    Added comments and user friendliness

--- DriveShareCheck/Program.cs
+++ DriveShareCheck/Program.cs
@@ -10,10 +10,11 @@ namespace DriveShareCheck
     internal class Program
     {
         private static List<String> dip = new List<string>(); //dip like desktop.ini paths
-        private static string startpath = $@"C:\Users\{Environment.UserName}\Google Drive";
+        private static readonly string startpath = $@"C:\Users\{Environment.UserName}\Google Drive";
 
         private static void Main(string[] args)
         {
+            Console.WriteLine($"Looking for Drive at {startpath}");
             IterateDirectories(startpath);
             foreach (var path in dip)
             {
@@ -22,15 +23,21 @@ namespace DriveShareCheck
             Console.ReadKey();
         }
 
+        //Recursive Iteration
         private static void IterateDirectories(string path)
         {
+            //Read all Directories and Files of current directory
             var dir = Directory.EnumerateDirectories(path);
             var files = Directory.EnumerateFiles(path);
+
+            //Check if it's not the main drive directory, else check if it has a desktop.ini file
             if (path != startpath && files.Any(a => a.EndsWith("desktop.ini")))
             {
                 dip.Add(path);
                 return;
             }
+
+            //Iterate and execute recursion
             foreach (var d in dir)
             {
                 IterateDirectories(d);
